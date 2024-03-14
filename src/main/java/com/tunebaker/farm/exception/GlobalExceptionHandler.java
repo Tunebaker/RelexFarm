dc2539098a84fb983289478ae61e53a13ce58317
Tunebaker <tunebaker@gmail.com>
2024-03-14 18:37:48 +0300

    update exception handler

--- src/main/java/com/tunebaker/farm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tunebaker/farm/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.tunebaker.farm.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,10 +14,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RuntimeException.class)
     @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> handleException(RuntimeException e) {
-        AppErrorInfo errorInfo = new AppErrorInfo().status(500)
+        int status;
+        switch (e) {
+            case DataIntegrityViolationException ex -> status = 400;
+            case UserAlreadyExistsException ex -> status = 400;
+            case PasswordsNotEqualException ex -> status = 400;
+            default -> status = 500;
+        }
+
+
+        AppErrorInfo errorInfo = new AppErrorInfo().status(status)
                                                    .date(new Date())
                                                    .exception(e.getClass().getName())
                                                    .message(e.getMessage());
-        return ResponseEntity.status(500).body(errorInfo.toString());
+        return ResponseEntity.status(status).body(errorInfo.toString());
     }
 }
