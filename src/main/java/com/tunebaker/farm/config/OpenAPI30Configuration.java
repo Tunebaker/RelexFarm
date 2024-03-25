ca72add291f13ac35c60133fe699b10fcf86b9e0
Tunebaker <tunebaker@gmail.com>
2024-03-25 18:51:33 +0300

    merge two openApi config classes to one

--- src/main/java/com/tunebaker/farm/config/OpenAPI30Configuration.java
+++ src/main/java/com/tunebaker/farm/config/OpenAPI30Configuration.java
@@ -1,10 +1,27 @@
 package com.tunebaker.farm.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info =@Info(
+                title = "Relex Farm user API",
+                version = "${api.version}",
+                contact = @Contact(
+                        name = "Andrey (developer)", email = "user-apis@tunebaker.com"
+                )
+        ),
+        servers = @Server(
+                url = "http://localhost:${server.port}",
+                description = "local"
+        )
+)
 @SecurityScheme(
         name = "Bearer Authentication",
         type = SecuritySchemeType.HTTP,
